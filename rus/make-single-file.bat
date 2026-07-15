ad42f5655c28b4ef1e821c88e8230db39127df69
Crazyviny <56147879+Crazyviny@users.noreply.github.com>
2026-07-15 16:01:12 +0700

    Normalize double line breaks in Russian text

--- rus/make-single-file.bat
+++ rus/make-single-file.bat
@@ -1,14 +1,10 @@
 @echo off
 setlocal enabledelayedexpansion
-
 set OUTPUT=../current-trans.md
-
 if exist "%OUTPUT%" del "%OUTPUT%"
-
 for /L %%N in (1,1,300) do (
     set NUM=00%%N
     set NUM=!NUM:~-3!
-
     if exist "!NUM!.md" (
         type "!NUM!.md" >> "%OUTPUT%"
         echo.>> "%OUTPUT%"
@@ -16,6 +12,5 @@ for /L %%N in (1,1,300) do (
         echo.>> "%OUTPUT%"
     )
 )
-
 echo Done: %OUTPUT%
 pause
